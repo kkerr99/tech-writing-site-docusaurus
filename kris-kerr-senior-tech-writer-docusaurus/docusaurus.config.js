3049ca9c1d499e2244418170f208fa074c3327eb
Kris Kerr <kkerr@kkerr-macbook-pro-24-ghz-core-i9.local>
2023-08-08 18:11:48 -0700

    Change baseUrl value

--- kris-kerr-senior-tech-writer-docusaurus/docusaurus.config.js
+++ kris-kerr-senior-tech-writer-docusaurus/docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://kris-kerr-senior-technical-writer.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/kris-kerr-senior-tech-writer-docusaurus/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
